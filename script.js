f5adb528e3dec2f0fabd39fa4ae46c3a9c6473ac
William Pognonec <william.pognonec@gmail.com>
2025-01-07 11:53:16 -0600

    Form validation and styling

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 const myLibrary = []
 const bookList = document.querySelector("#bookList")
 const dialog = document.querySelector("#addBookDialog")
+const title = document.querySelector("#title")
+const author = document.querySelector("#author")
+const pages = document.querySelector("#pages")
 
 function Book(title, author, pages, read = false) {
   this.title = title
   this.author = author
   this.pages = pages
   this.read = read
-  this.info = function() {
-    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "read" : "not read"}`;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${
+      this.read ? "read" : "not read"
+    }`
   }
 }
 
@@ -22,7 +27,6 @@ function addBookToLibrary(title, author, pages, read) {
 }
 
 function displayBooks() {
-
   // Delete the book list
   bookList.replaceChildren()
 
@@ -50,12 +54,16 @@ function displayBooks() {
 
     // Add event listeners to del and read buttons
     delButton.addEventListener("click", (e) => {
-      const bookId = Number(e.target.parentElement.parentElement.attributes["data-id"].value)
+      const bookId = Number(
+        e.target.parentElement.parentElement.attributes["data-id"].value
+      )
       myLibrary.splice(bookId, 1)
       displayBooks()
     })
     readButton.addEventListener("click", (e) => {
-      const bookId = Number(e.target.parentElement.parentElement.attributes["data-id"].value)
+      const bookId = Number(
+        e.target.parentElement.parentElement.attributes["data-id"].value
+      )
       myLibrary[bookId].readToggle()
       displayBooks()
     })
@@ -69,8 +77,34 @@ function showDialog() {
 dialog.addEventListener("submit", (e) => {
   let data = Object.fromEntries(new FormData(e.target))
   e.target.reset()
-  addBookToLibrary(data.title, data.author, Number(data.pages), Boolean(data.read))
+  addBookToLibrary(
+    data.title,
+    data.author,
+    Number(data.pages),
+    Boolean(data.read)
+  )
   displayBooks()
+})
+
+title.addEventListener("invalid", () => {
+  title.setCustomValidity("Please enter a title")
+})
+title.addEventListener("input", () => {
+  title.setCustomValidity("")
+})
+
+author.addEventListener("invalid", () => {
+  author.setCustomValidity("Please enter an author")
+})
+author.addEventListener("input", () => {
+  author.setCustomValidity("")
+})
+
+pages.addEventListener("invalid", () => {
+  pages.setCustomValidity("Please enter the number of pages")
+})
+pages.addEventListener("input", () => {
+  pages.setCustomValidity("")
 })
 
 addBookToLibrary("Project Hail Mary", "Andy Weir", 476, true)
